9c0fa7f224bea88c724fdca53e30569c52378827
h3ll <abdelrahman20191919@gmail.com>
2025-02-15 01:39:03 +0200

    yay bash for building shitgit add *! totally recommend doing itgit add *!

--- build.sh
+++ build.sh
@@ -1,1 +1,29 @@
-echo "hi world"
+#!/bin/bash
+
+# use in root dir pls:pray:
+
+withbear="true"
+openafterbuild="true"
+executablename="rte.exe"
+
+if [ ! -d build ]; then
+  mkdir build
+fi
+
+if [ "$withbear" == "false" ]; then
+  gcc -Wall -Werror -Iinclude -o $executablename $(find src -type f -name "*.c") -Llib
+elif [ "$withbear" == "true" ]; then
+  bear -- gcc -Wall -Werror -Iinclude -o $executablename $(find src -type f -name "*.c") -Llib
+else
+  echo "withbear : $withbear has to be either \"true\" or \"false\" "
+fi
+
+echo "built"
+
+if [ "$openafterbuild" == "false" ]; then
+  echo "done"
+elif [ "$openafterbuild" == "true" ]; then
+  ./$executablename
+else
+  echo "openafterbuild : $openafterbuild has to be either \"true\" or \"false\" "
+fi
